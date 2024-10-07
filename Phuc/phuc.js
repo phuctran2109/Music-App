44d846ee4fb36fb5874e7f8d4bbbfde0e161c2be
phuctran2109 <130390162+phuctran2109@users.noreply.github.com>
2024-10-07 18:35:15 +0700

    testGit

---
+++ Phuc/phuc.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const phuc = () => {
+  return (
+    <View>
+      <Text>phuc</Text>
+    </View>
+  )
+}
+
+export default phuc
+
+const styles = StyleSheet.create({})
